8c9972df56da41f5780ced4c82d58a352d213c57
emanuel <emanuelbotelho@ymail.com>
2020-05-31 15:28:53 -0300

    Project created

---
+++ src/components/MainApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-5 container">
+    <h1>Einstein Test</h1>
+    <p class="ml-1">How fast and smart you are?</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    base() {
+      return {
+
+      }
+    }
+  }
+</script>
